f463c2d4cbb322bcfc2c998d996e64b9e67dd6d3
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2023-08-22 23:11:34 +0530

    updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,7 +29,7 @@ export const Navbar = () => {
         <img src="/search.svg" alt="search icon" />
         <div className=" notification">
           <img src="/bell.svg" alt="" />
-          <span>1</span>
+          {/* <span>1</span> */}
         </div>
         <div className="user">
           <img src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="user image" />
